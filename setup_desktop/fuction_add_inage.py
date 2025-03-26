8aaed74561c82a2199f8e7c027964bc26f7ef33b
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-26 13:09:58 -0300

    feat: add to project

--- setup_desktop/fuction_add_inage.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-from time import sleep
-
-
-def add_image(img):
-    image = 'imgs/'+ img + '.png'
-    sleep(1)
-    local_image = pyautogui.locateOnScreen(image, confidence=0.9)
-    return pyautogui.click(local_image)
