0087739ab2e9d4209f1a9a50dc65def682ddcc49
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-26 12:50:10 -0300

    feat: add image

--- setup_desktop/main.py
+++ setup_desktop/main.py
@@ -3,7 +3,11 @@ import os
 from time import sleep
 from dotenv import load_dotenv
 
+from setup_desktop.fuction_add_inage import add_image
+
 load_dotenv()
 
 SENHA_NOTION = os.getenv("SENHA_NOTION")
 SENHA_GOOGLE = os.getenv("SENHA_GOOGLE")
+
+add_image('icone_google')
