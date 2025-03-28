4651e17dbd29af5c4b3c6fe2f714af75b31b754c
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-28 15:45:26 -0300

    feat: prjeto asimov news

--- web_news/scraping_sites/site.py
+++ web_news/scraping_sites/site.py
@@ -1,34 +1,109 @@
 import requests
 from bs4 import BeautifulSoup
 
-class SiteNoticias:
-    def __init__(self, site, header_tags, class_names):
-        """
-        site: Nome descritivo do site (por exemplo, 'globo', 'uol')
-        header_tags: Lista de possíveis tags de cabeçalho que encapsulam os títulos (ex: ['h2', 'h3'])
-        class_names: Lista de possíveis nomes de classes que identificam títulos de notícias
-        """
-        self.site = site
-        self.header_tags = header_tags
-        self.class_names = class_names
-        self.news = []
+class Site:
+    def __init__ (self, opcao:str):
+        self.opcao = opcao
 
-    def update_news(self, url):
-        browsers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-        page = requests.get(url, headers=browsers)
-        if page.status_code != 200:
-            raise Exception(f"Failed to load page from {url}")
-        
-        resposta = page.text
-        soup = BeautifulSoup(resposta, 'html.parser')
-        noticias = soup.find_all('a')
+    def update_news(self):
+        if self.opcao.lower() == 'globo':
+            url = 'https://www.globo.com/'
+            browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \(KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
+            page = requests.get(url, headers = browsers)
 
-        # Coletar notícias com base em diferentes tags e classes
-        news_dict = {}
-        for noticia in noticias:
-            for tag in self.header_tags:
-                header = noticia.find(tag)
-                if header and any(cls in header.get('class', []) for cls in self.class_names):
-                    # Armazenar título com link
-                    news_dict[header.text] = noticia.get('href')
-        self.news = news_dict
+            resposta = page.text
+            soup = BeautifulSoup(resposta, 'html.parser')
+            noticias = soup.find_all('a')
+
+            target_class_1 = 'post__title'
+            target_class_2 = 'post-multicontent__link--title__text'
+
+            news_dict_globo = {}
+
+            for noticia in noticias:       
+                if (noticia.h2 != None) and (noticia.h2.get('class') != None):
+                    if target_class_1 in noticia.h2.get('class'):
+                        news_dict_globo[noticia.h2.text] = noticia.get('href')
+                    if target_class_2 in noticia.h2.get('class'):
+                        news_dict_globo[noticia.h2.text] = noticia.get('href')
+            self.news = news_dict_globo
+
+        if self.opcao.lower() == 'veja':
+            url = 'https://veja.abril.com.br/'
+            browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \(KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
+            page = requests.get(url, headers = browsers)
+
+            resposta = page.text
+            soup = BeautifulSoup(resposta, 'html.parser')
+            noticias = soup.find_all('a')
+
+            target_class_1 = 'related-article'
+            target_class_2 = 'title'
+
+            news_dict_veja = {}
+
+            for noticia in noticias:
+                if (noticia.get('class') != None) and (target_class_1 in noticia.get('class')):
+                    news_dict_veja[noticia.text] = noticia.get('href')
+                if (noticia.h2 != None) and (noticia.h2.get('class') != None) and (target_class_2 in noticia.h2.get('class')):
+                    news_dict_veja[noticia.h2.text] = noticia.get('href')
+                if (noticia.h3 != None) and (noticia.h3.get('class') != None) and (target_class_2 in noticia.h3.get('class')):
+                    news_dict_veja[noticia.h3.text] = noticia.get('href')
+                if (noticia.h4 != None) and (noticia.h4.get('class') != None) and (target_class_2 in noticia.h4.get('class')):
+                    news_dict_veja[noticia.h4.text] = noticia.get('href')
+            self.news =  news_dict_veja
+
+        if self.opcao.lower() == 'r7':
+            url = 'https://www.r7.com/'
+            browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \(KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
+            page = requests.get(url, headers = browsers)
+
+            resposta = page.text
+            soup = BeautifulSoup(resposta, 'html.parser')
+            noticias = soup.find_all('a')
+
+            target_class_1 = 'r7-flex-title-h4__link'
+            target_class_2 = 'r7-flex-title-h5__link'
+            target_class_3 = 'r7-flex-title-h6__link'
+
+            news_dict_r7 = {}          
+                        
+            for i in range(len(noticias)):
+                if noticias[i].get('class') != None:
+                    if target_class_1 in noticias[i].get('class'):
+                        news_dict_r7[noticias[i].text.split("\n")[1].strip()] = noticias[i].get('href')
+                    if target_class_2 in noticias[i].get('class'):
+                        news_dict_r7[noticias[i].text.split("\n")[1].strip()] = noticias[i].get('href')
+                    if target_class_3 in noticias[i].get('class'):
+                        news_dict_r7[noticias[i].text.split("\n")[1].strip()] = noticias[i].get('href')
+
+            self.news = news_dict_r7
+
+        if self.opcao.lower() == 'cnn':
+            url = 'https://www.cnnbrasil.com.br/'
+            browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \(KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
+            page = requests.get(url, headers = browsers)
+
+            resposta = page.text
+            soup = BeautifulSoup(resposta, 'html.parser')
+            noticias = soup.find_all('a')
+
+            target_class_1 = 'headline__primary_title'
+            target_class_2 = 'home__title'
+            target_class_3 = 'articles__title'
+            target_class_4 = 'webstories_title'
+            news_dict_cnn = {}
+
+            for i in range(len(noticias)):
+                if (noticias[i].h2 != None) and (noticias[i].h2.get('class') != None) and (target_class_1 in noticias[i].h2.get('class')):
+                    news_dict_cnn[noticias[i].h2.text] = noticias[i].get('href')
+                if noticias[i].h3 != None and (noticias[i].h3.get('class') != None):
+                    if target_class_2 in noticias[i].h3.get('class'):
+                        news_dict_cnn[noticias[i].h3.text] = noticias[i].get('href')
+                    if target_class_3 in noticias[i].h3.get('class'):
+                        news_dict_cnn[noticias[i].h3.text] = noticias[i].get('href')
+                    if target_class_4 in noticias[i].h3.get('class'):
+                        news_dict_cnn[noticias[i].h3.text] = noticias[i].get('href')
+
+            self.news = news_dict_cnn
+
